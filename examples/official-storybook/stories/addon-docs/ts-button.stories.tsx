e552e7ad64259152ad3a98e2be1603ce8680f888
Andrew Lisowski <lisowski54@gmail.com>
2020-05-04 11:24:37 -0700

    show bug

--- examples/official-storybook/stories/addon-docs/ts-button.stories.tsx
+++ examples/official-storybook/stories/addon-docs/ts-button.stories.tsx
@@ -2,16 +2,27 @@ import * as React from 'react';
 import { action } from '@storybook/addon-actions';
 import { radios } from '@storybook/addon-knobs';
 import Button, { Type } from '../../components/TsButton';
+import allMdx from './all.mdx';
 
 export default {
   title: 'Addons/Docs/TsButton',
   component: Button,
-  parameters: {
-    viewMode: 'story',
-  },
 };
 
 type Story = () => any;
+
+export const AllStory = () => <div>Navigate to the docs tab for content</div>;
+
+AllStory.story = {
+  parameters: {
+    docs: {
+      page: allMdx,
+    },
+    previewTabs: {
+      canvas: { hidden: true },
+    },
+  },
+};
 
 export const SimpleButton: Story = () => {
   const x = 0;
